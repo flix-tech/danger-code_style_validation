87f3182be9ac45d9247f617df246c32935c3fb15
Nikolay Kasyanov <nikolay.kasyanov@flixbus.com>
2017-04-28 14:17:00 +0200

    Replaces GitLab with GitHub in spec

--- spec/code_style_validation_spec.rb
+++ spec/code_style_validation_spec.rb
@@ -15,7 +15,7 @@ module Danger
       it 'Reports code style violation as error' do
         diff = File.read('spec/fixtures/violated_diff.diff')
 
-        @my_plugin.gitlab.stub(:mr_diff).and_return diff
+        @my_plugin.github.stub(:pr_diff).and_return diff
         @my_plugin.check
 
         expect(@dangerfile.status_report[:errors]).to eq([DangerCodeStyleValidation::VIOLATION_ERROR_MESSAGE])
@@ -24,7 +24,7 @@ module Danger
       it 'Does not report error when code not violated' do
         diff = File.read('spec/fixtures/innocent_diff.diff')
 
-        @my_plugin.gitlab.stub(:mr_diff).and_return diff
+        @my_plugin.github.stub(:pr_diff).and_return diff
         @my_plugin.check
 
         expect(@dangerfile.status_report[:errors]).to eq([])
@@ -33,7 +33,7 @@ module Danger
       it 'Does not report error for different extension types of files' do
         diff = File.read('spec/fixtures/ruby_diff.diff')
 
-        @my_plugin.gitlab.stub(:mr_diff).and_return diff
+        @my_plugin.github.stub(:pr_diff).and_return diff
         @my_plugin.check
 
         expect(@dangerfile.status_report[:errors]).to eq([])
@@ -42,7 +42,7 @@ module Danger
       it 'Does not report unexpected errors when there are only removals in the diff' do
         diff = File.read('spec/fixtures/red_diff.diff')
 
-        @my_plugin.gitlab.stub(:mr_diff).and_return diff
+        @my_plugin.github.stub(:pr_diff).and_return diff
         @my_plugin.check
 
         expect(@dangerfile.status_report[:errors]).to eq([])
@@ -51,7 +51,7 @@ module Danger
       it 'Ignores files matching ignored patterns' do
         diff = File.read('spec/fixtures/violated_diff.diff')
 
-        @my_plugin.gitlab.stub(:mr_diff).and_return diff
+        @my_plugin.github.stub(:pr_diff).and_return diff
         @my_plugin.check ignore_file_patterns: [%r{^spec/}]
 
         expect(@dangerfile.status_report[:errors]).to eq([])
@@ -60,7 +60,7 @@ module Danger
       it 'Allows single pattern instead of array' do
         diff = File.read('spec/fixtures/violated_diff.diff')
 
-        @my_plugin.gitlab.stub(:mr_diff).and_return diff
+        @my_plugin.github.stub(:pr_diff).and_return diff
         @my_plugin.check ignore_file_patterns: %r{^spec/}
 
         expect(@dangerfile.status_report[:errors]).to eq([])
